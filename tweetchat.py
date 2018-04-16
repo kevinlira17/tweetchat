806d2a0351127e84036f5a029df77d2fe9b86390
AscendingSnow <37675315+AscendingSnow@users.noreply.github.com>
2018-04-16 18:40:27 -0400

    Update tweetchat.py
    filters twitter_stream by emoji and language. Geo-location filtering not implemented yet.

--- tweetchat.py
+++ tweetchat.py
@@ -1,3 +1,4 @@
+
 # Import the necessary package to process data in JSON format
 try:
     import json
@@ -8,10 +9,10 @@ except ImportError:
 from twitter import *
 
 # Variables that contains the user credentials to access Twitter API 
-access_token = 'ACCESS_TOKEN'
-access_token_secret = 'ACCESS_TOKEN_SECRET'
-consumer_key = 'CONSUMER_KEY'
-consumer_key_secret = 'CONSUMER_KEY_SECRET'
+access_token = '64570827-oNAvcxk9F9y9tplfENm5faCmpcLaO5mIqsav3SMPL'
+access_token_secret = 'MT8NLOrAo2uT9481V8IsXpYug30Ri5vyVsCprxE57zNRI'
+consumer_key = 'SJsvNkN8SkbYZNdzisPiyXi0D'
+consumer_key_secret = '5SiP0nWhCHmwhOEEwC30799GKPuRV6egblGW3Ov1EFUUqpGa55'
 
 #oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
 
@@ -19,22 +20,31 @@ consumer_key_secret = 'CONSUMER_KEY_SECRET'
 twitter_stream = TwitterStream(domain='stream.twitter.com', auth=OAuth(access_token, access_token_secret, consumer_key,  consumer_key_secret))
 
 # Get a sample of the public data following through Twitter
-iterator = twitter_stream.statuses.sample()
+
+# copy and paste these: 🙂🙁👍👎
+iterator = twitter_stream.statuses.filter(track="🙂", language="en")
 
 # Print each tweet in the stream to the screen 
 # Here we set it to stop after getting 1000 tweets. 
 # You don't have to set it to stop, but can continue running 
 # the Twitter API to collect data for days or even longer. 
-tweet_count = 5
+tweet_count = 1
 for tweet in iterator:
-    tweet_count -= 1
     # Twitter Python Tool wraps the data returned by Twitter 
     # as a TwitterDictResponse object.
     # We convert it back to the JSON format to print/score
-    print(json.dumps(tweet))
+    stringtweet = json.dumps(tweet)
+    data = json.loads(stringtweet)
+    if "text" in data:
+        text = data["text"]
+        # we want no overlap with out tweets
+        if "🙁" not in text and "👍" not in text and "👎" not in text and "🙂" in text:
+            print(text)
+            # save to file placeholder
+            tweet_count -= 1
     
     # The command below will do pretty printing for JSON data, try it out
-    print(json.dumps(tweet, indent=4))
+    #print(json.dumps(tweet, indent=4))
        
     if tweet_count <= 0:
         break 
