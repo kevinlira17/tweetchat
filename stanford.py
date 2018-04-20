0ad66027f512d8cd8881d99bdaf8e5b386778bb9
AscendingSnow <37675315+AscendingSnow@users.noreply.github.com>
2018-04-20 18:43:29 -0400

    writes individual line scores for each tweet

--- stanford.py
+++ stanford.py
@@ -3,11 +3,18 @@
 # REQUIRES PIP INSTALL STANFORDCORENLP AND 'STANFORD-CORENLP-FULL-2018-02-27'
 # (FREE) IN DIRECTORY
 
+# 1.754 for frown without emoji
+# 1.657 with frown emoji???
+# 2.02 with smile emoji
+# 2.136 without smile emoji ???
+
+
 import os
 import json
 from stanfordcorenlp import StanfordCoreNLP
 
-f = open("cleanFrown.txt", "r", encoding="utf8")
+f = open("cleanSmile.txt", "r", encoding="utf8")
+p = open("smileClassificationWithEmoji.txt", "w", encoding="utf8")
 
 #start it up
 dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -19,13 +26,15 @@ totalSentimentValue = 0
 nDoneSoFar = 0
 for line in f:
     try:
+        sentenceSentimentValue = 0
         res = nlp.annotate(line, properties={'annotators': 'sentiment'})
-        print(res)
         asjson = json.loads(res)
 
         for s in asjson['sentences']:
             nDoneSoFar += 1
-            totalSentimentValue += int(s['sentimentValue'])
+            sentenceSentimentValue += int(s['sentimentValue'])
+        p.write(str(sentenceSentimentValue) + "\t" + line)
+        totalSentimentValue += sentenceSentimentValue
             
     except ValueError:
         print("gah oh no")
@@ -33,5 +42,8 @@ for line in f:
 #shut it down
 nlp.close()
 f.close()
+p.close()
 
+print("Number of tweets: " + str(nDoneSoFar))
 print("Average sentiment value: " + str(totalSentimentValue / nDoneSoFar))
+
