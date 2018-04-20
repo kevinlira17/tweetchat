9bee6aeb78fe20c41b8233038234b680f6e53c8c
AscendingSnow <37675315+AscendingSnow@users.noreply.github.com>
2018-04-20 18:42:30 -0400

    option to remove emoji and more stopwords

--- cleandata.py
+++ cleandata.py
@@ -4,17 +4,19 @@ import nltk
 from nltk.corpus import stopwords
 
 stoplist = stopwords.words('english')
-stoplist.extend([".", ",", ";", ":", "“", "”", "'", "?", "!", "--", "’", "(", ")", "I", "It", "The", "And", "i", "n't", "'ve", "'d", "'s", "@", "#"])
+stoplist.extend([".", ",", ";", ":", "“", "”", "'", "?", "!", "``", "''", "...", "-", "--", "’", "(", ")", "I", "It", "The", "And", "i", "n't", "'ve", "'d", "'s", "@", "#", "_", "__"])
 
-f = open("input.txt", "r", encoding="utf8")
+f = open("smileTweetsRaw.txt", "r", encoding="utf8")
 
 tweets = ''
 for line in f:
-    line = ' '.join(re.sub("(@[A-Za-z0-9]+)|(_[0-9A-Za-z]+)|(#[0-9A-Za-z]+)"," ",line).split())
-    linetokens = nltk.word_tokenize(line)
-    noStopwords = [token for token in linetokens if token not in stoplist]
-    noStopwordsString = ' '.join(noStopwords)
-    tweets += (noStopwordsString + "\n")
+    if len(line) > 3:
+        # add |([^0-9A-Za-z \t]) to get no emoji
+        line = ' '.join(re.sub("(@[A-Za-z0-9]+)|(_[0-9A-Za-z_]+)|(#[0-9A-Za-z]+)"," ",line).split())
+        linetokens = nltk.word_tokenize(line)
+        noStopwords = [token for token in linetokens if token not in stoplist]
+        noStopwordsString = ' '.join(noStopwords)
+        tweets += (noStopwordsString + "\n")
 f.close()
 
 p = open("cleanSmile.txt", "w", encoding="utf8")
