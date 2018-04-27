ede3c330b54da74cb8e8ec41861467d2baa66fd7
kevinlira17 <leebkf@bc.edu>
2018-04-26 20:33:10 -0400

    fixed LIWC implementation

--- extractfile.py
+++ extractfile.py
@@ -1,12 +1,18 @@
 import sys
 import os
 
+def checkTweetSize(tweetdata):
+    with open(tweetdata, 'r', encoding="utf-8") as f:
+        for i, l in enumerate(f):
+            pass
+    print(i + 1)
+
 def extractData(tweetdata):
     non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
     with open(tweetdata, 'r', encoding="utf-8") as readfile:
         count = 1
         for line in readfile:
-            with open(os.path.join('/Users/kevinlira/NLP/Testdata',"test" + str(count) + ".txt"), 'w', encoding="utf-8") as f:
+            with open(os.path.join('/Users/kevinlira/NLP/Testdata', str(count) + ".txt"), 'w', encoding="utf-8") as f:
                 f.write(line)
                 print(line.translate(non_bmp_map))
             count += 1
@@ -14,23 +20,26 @@ def extractData(tweetdata):
 def formatResult(tweetdata, resdata):
     non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
     tweetlist = []
-    reslist = []
+    resmap = {}
     with open(tweetdata, 'r', encoding="utf-8") as readfile:
         for line in readfile:
             tweetlist.append(line)
     with open(resdata, 'r', encoding="utf-8") as readfile:
         for line in readfile:
             words = line.split()
+            fileval = words[0].strip(".txt")
             if (words[2] != "posemo"):
                 if (float(words[2]) > float(words[3])):
-                    reslist.append("pos")
+                    resmap[fileval] = "1"
                 elif(float(words[2]) < float(words[3])):
-                    reslist.append("neg")
+                    resmap[fileval] = "0"
                 else:
-                    reslist.append("neu")
+                    resmap[fileval] = "-1"
     with open("resdata.txt", 'w', encoding="utf-8") as save:
         for i in range(len(tweetlist)):
-            save.write(reslist[i] + " " + tweetlist[i] + "\n")
+            save.write(resmap[str(i+1)] + " " + tweetlist[i] + "\n")
+    print("complete")
 
+#checkTweetSize("cleanTestData.txt")
 #extractData("cleanTestData.txt")
 #formatResult("cleanTestData.txt", "LIWC2015res.txt")
